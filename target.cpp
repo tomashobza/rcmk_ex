b65dd8cf87ebc38ee051d1b94f7f5c1e774a5073
Tomáš Hobza <hobza.tomas@gmail.com>
2026-01-17 18:57:51 +0100

    add: save sim_time at struct tail to prevent torn reads

--- target.cpp
+++ target.cpp
@@ -4,38 +4,40 @@
 #include <algorithm>
 
 struct Telemetry {
-    uint32_t sim_time;
+    uint32_t sim_time_start;
     uint16_t speed;
     uint16_t rpm;
     uint8_t gear;
+    uint32_t sim_time_end;
 };
 
-void printTelemetry(Telemetry telem, int sim_time) {
+void printTelemetry(Telemetry telem) {
     std::cout << "====" << std::endl;
-    std::cout << "sim_time: " << sim_time << std::endl;
+    std::cout << "sim_time: " << telem.sim_time_start << std::endl;
     std::cout << "\tspeed: " << telem.speed << std::endl;
     std::cout << "\trpm: " << telem.rpm << std::endl;
     std::cout << "\tgear: " << (int)telem.gear << std::endl;
 }
 
-void updateTelemetry(Telemetry &telem, int sim_time) {
+void updateTelemetry(Telemetry &telem, uint32_t sim_time) {
+    telem.sim_time_start = sim_time; // add current simulation_time
     int random_acc = rand() % 4 - 2; // <-2,2> random int
     int new_speed = (int)telem.speed + random_acc;
     telem.speed = (uint16_t)std::clamp(new_speed, 0, 400);
     telem.gear = 1 + (telem.speed / 20); // every 20km/h change the gear
     telem.rpm = (telem.speed % 20) * 300; // map speed in gear to rpm
-    telem.sim_time = sim_time; // add current simulation_time
+    telem.sim_time_end = sim_time; // add current simulation_time adn the tail for preventing torn reads
 }
 
 int main() {
-    int sim_time = 0; // simulation time
+    uint32_t sim_time = 0; // simulation time
 
     // car telemetry
     Telemetry telem = {
         .speed = 50, // initial speed
     };
 
-    updateTelemetry(telem);
+    updateTelemetry(telem, sim_time);
 
     std::cout << "[Starting target app]" << std::endl;
     std::cout << "PID: " << GetCurrentProcessId() << std::endl;
@@ -47,7 +49,7 @@ int main() {
         ++sim_time;
 
         // update telemetry
-        updateTelemetry(telem);
+        updateTelemetry(telem, sim_time);
 
         printTelemetry(telem, sim_time);
 
