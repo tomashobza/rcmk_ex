fcc9b18187de2dcd1f60e7d86454790b7fce4f60
Tomáš Hobza <hobza.tomas@gmail.com>
2026-01-20 01:47:34 +0100

    update: target hittin zero speed to often

--- target.cpp
+++ target.cpp
@@ -22,10 +22,15 @@ void printTelemetry(Telemetry telem) {
 void updateTelemetry(Telemetry &telem, uint32_t sim_time) {
     telem.sim_time_start = sim_time; // add current simulation_time
     int random_acc = rand() % 4 - 2; // <-2,2> random int
-    int new_speed = (int)telem.speed + random_acc;
+    int new_speed = 0;
+    if (telem.speed + random_acc < 0) {
+        new_speed = telem.speed - random_acc;
+    } else {
+        new_speed = telem.speed + random_acc;
+    }
     telem.speed = (uint16_t)std::clamp(new_speed, 0, 400);
     telem.gear = 1 + (telem.speed / 20); // every 20km/h change the gear
-    telem.rpm = (telem.speed % 20) * 300; // map speed in gear to rpm
+    telem.rpm = (telem.speed % 20) * 200 + 1000; // map speed in gear to rpm
     telem.sim_time_end = sim_time; // add current simulation_time adn the tail for preventing torn reads
 }
 
